c8ccdbb81468aa7215bd00cb76a878980e59a476
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-02-14 18:25:48 +0200

    accepts large number of arguments

--- mycat.c
+++ mycat.c
@@ -6,18 +6,27 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	char buffer[1024];
-	int ReadFile;
-	int WriteFile;
 
-	/* open input and output files */
-	ReadFile = open(argv[1], O_RDONLY);
-	WriteFile = open(argv[2], O_CREAT|O_WRONLY|O_APPEND, 0600);
-	
-	/* read and write byte by byte */
-	/* read returns 0 when EOF - END OF FILE */
-	while ( read(ReadFile, buffer, 1) > 0 ) { 
+	for (int i = 1; i < argc; i++) {
+	   
+	   /* final argument optimization */
+	   if ( i+1 > argc-1 ) { 
+		return 0;
+	   }
+
+	   char buffer[1024];
+	   int ReadFile;
+	   int WriteFile;
+
+	   /* open input and output files */
+	   ReadFile = open(argv[i], O_RDONLY);
+	   WriteFile = open(argv[i+1], O_CREAT|O_WRONLY|O_APPEND,0600);
+
+	   /* read and write byte by byte */
+	   /* read returns 0 when EOF - END OF FILE */
+	   while ( read(ReadFile, buffer, 1) > 0 ) { 
 		write(WriteFile, buffer, 1);
+	   }
 	}
 
 }
