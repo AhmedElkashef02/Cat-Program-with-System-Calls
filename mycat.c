6fc47983bd6d794debd4d46360c4d7d86308fc8f
Ahmed Elkashef <ahmedelkashef2012@gmail.com>
2018-02-14 17:54:36 +0200

    read and write byte by byte

--- mycat.c
+++ mycat.c
@@ -6,15 +6,18 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	char buffer[11];
+	char buffer[1024];
 	int ReadFile;
 	int WriteFile;
 
+	/* open input and output files */
 	ReadFile = open("input.txt", O_RDONLY);
-	read(ReadFile, buffer, 11);
-	close(ReadFile);
-
 	WriteFile = open("output.txt", O_CREAT | O_WRONLY, 0600);
-	write(WriteFile, buffer, 11);
-	close(WriteFile);
+	
+	/* read and write byte by byte */
+	/* read returns 0 when EOF - END OF FILE */
+	while ( read(ReadFile, buffer, 1) > 0 ) { 
+		write(WriteFile, buffer, 1);
+	}
+
 }
